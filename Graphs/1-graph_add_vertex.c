ec6c30d400aadb06f9a86b15cb3825b2c29a769d
alaedine <boufaden.alaeddine.dev@gmail.com>
2023-08-09 01:23:27 +0100

    graph add vertex

---
+++ Graphs/1-graph_add_vertex.c
@@ -0,0 +1,43 @@
+#include "graphs.h"
+
+vertex_t *graph_add_vertex(graph_t *graph, const char *str);
+
+vertex_t *graph_add_vertex(graph_t *graph, const char *str)
+{
+    /* if graph has no vertex then create adjacency linked list */
+    char *new_string = malloc(sizeof(strlen(str)));
+    int index = 0;
+    if (graph->nb_vertices == 0)
+    {
+        /* create node which will be the head of linked list */
+        vertex_t *node = malloc(sizeof(vertex_t));
+        node->index = 0;
+        node->content = strcpy(new_string, str);
+        node->nb_edges = 0;
+        node->edges = NULL;
+        node->next = NULL;
+        graph->vertices = node;
+        graph->nb_vertices = 1;
+        return node;
+    }
+    /* traverse the adjacency linked list and insert another vertex that holds the str */
+    else
+    {
+        vertex_t *current = malloc(sizeof(vertex_t));
+        current = graph->vertices;
+        while (current->next != NULL)
+        {
+            index = current->index;
+            current = current->next;
+        }
+        vertex_t *node = malloc(sizeof(vertex_t));
+        node->index = index + 1;
+        node->content = strcpy(new_string, str);
+        node->nb_edges = 0;
+        node->edges = NULL;
+        node->next = NULL;
+        current->next = node;
+        graph->nb_vertices = index + 1;
+        return node;
+    }
+}
--- Graphs/graphs.h
+++ Graphs/graphs.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * enum edge_type_e - Enumerates the different types of
@@ -68,5 +69,7 @@ typedef struct graph_s
 } graph_t;
 
 graph_t *graph_create(void);
+vertex_t *graph_add_vertex(graph_t *graph, const char *str);
+void graph_display(const graph_t *graph);
 
 #endif
