98ca3d2decd7eecd7db27773152e0e9a7b53ccca
alaedine <boufaden.alaeddine.dev@gmail.com>
2023-08-21 20:08:58 +0100

    fix graph add vertex

--- Graphs/1-graph_add_vertex.c
+++ Graphs/1-graph_add_vertex.c
@@ -6,7 +6,6 @@ vertex_t *graph_add_vertex(graph_t *graph, const char *str)
 {
     /* if graph has no vertex then create adjacency linked list */
     char *new_string = malloc(sizeof(strlen(str)));
-    int index = 0;
     if (graph->nb_vertices == 0)
     {
         /* create node which will be the head of linked list */
@@ -27,17 +26,16 @@ vertex_t *graph_add_vertex(graph_t *graph, const char *str)
         current = graph->vertices;
         while (current->next != NULL)
         {
-            index = current->index;
             current = current->next;
         }
         vertex_t *node = malloc(sizeof(vertex_t));
-        node->index = index + 1;
+        node->index = current->index + 1;
         node->content = strcpy(new_string, str);
         node->nb_edges = 0;
         node->edges = NULL;
         node->next = NULL;
         current->next = node;
-        graph->nb_vertices = index + 1;
+        graph->nb_vertices = node->index + 1;
         return node;
     }
 }
